870a04c608af17c68cb0aa25e652c6c1dde6ab2d
n1ru4l <laurinquast@googlemail.com>
2020-11-24 14:49:56 +0100

    docs: add some information about the behaviour of the PushPullAsyncIterableIterator

--- src/PushPullAsyncIterableIterator.ts
+++ src/PushPullAsyncIterableIterator.ts
@@ -1,3 +1,12 @@
+const NO_ERROR_SYMBOL = Symbol("NO_ERROR_SYMBOL");
+
+/**
+ * PushPullAsyncIterableIterator
+ *
+ * The iterable will publish values until return ot throw is called.
+ * Afterwards it is in the completed state and cannot be used for publishing any further values.
+ * It will handle back-pressure and keep pushed values until they are consumed by a source.
+ */
 export class PushPullAsyncIterableIterator<T>
   implements AsyncIterableIterator<T> {
   private pushQueue: T[] = [];
@@ -6,18 +15,21 @@ export class PushPullAsyncIterableIterator<T>
     (value: unknown) => void
   ][] = [];
   private isRunning: boolean = true;
-  private errorValue: unknown;
+  private errorValue: unknown = NO_ERROR_SYMBOL;
 
+  /**
+   * Consume the next available value published on the iterable.
+   * It will resolve with a value immediately (if any is present in the queue).
+   * Otherwise, if no value is present, the subscriber will receive the next value once it is available.
+   */
   public async next(): Promise<IteratorResult<T>> {
     return new Promise((resolve, reject) => {
-      if (this.pushQueue.length !== 0) {
-        if (this.errorValue) {
-          reject(this.errorValue);
-          this.isRunning = false;
-          this.pullQueue.length = 0;
-          this.pushQueue.length = 0;
+      if (this.pushQueue.length !== 0 || this.isRunning === false) {
+        if (this.errorValue !== NO_ERROR_SYMBOL) {
+          this._handleError(reject);
           return;
         }
+
         resolve(
           this.isRunning
             ? { value: this.pushQueue.shift()!, done: false }
@@ -29,6 +41,10 @@ export class PushPullAsyncIterableIterator<T>
     });
   }
 
+  /**
+   * Complete a iterable. All pending consumer will receive a complete IteratorResult.
+   * The iterable cannot be used to publish any more values after completing it.
+   */
   public async return(): Promise<IteratorResult<T>> {
     if (this.isRunning) {
       this.isRunning = false;
@@ -41,18 +57,47 @@ export class PushPullAsyncIterableIterator<T>
     return { value: undefined, done: true };
   }
 
+  /**
+   * Throw a value and complete the iterable. After calling throw the next consumer will receive the error.
+   * All further consumers will receive a complete IteratorResult.
+   */
+  public throw(value: unknown): Promise<IteratorResult<T>> {
+    if (this.isRunning) {
+      this.errorValue = value;
+    }
+
+    return Promise.resolve({
+      done: true,
+      value: undefined
+    });
+  }
+
   public [Symbol.asyncIterator]() {
     return this;
   }
 
+  private _handleError(reject: (err: unknown) => void) {
+    reject(this.errorValue);
+    this.isRunning = false;
+    this.pullQueue.length = 0;
+    this.pushQueue.length = 0;
+    this.errorValue = NO_ERROR_SYMBOL;
+  }
+
+  /**
+   * Push new values into the iterator. If any consumers are present it will be forwarded to the consumers.
+   * Otherwise, the value will be saved until it is consumed.
+   */
   public push(value: T) {
+    if (this.isRunning === false) {
+      // TODO: Should this throw?
+      return;
+    }
+
     if (this.pullQueue.length > 0) {
       const [resolve, reject] = this.pullQueue.shift()!;
-      if (this.errorValue) {
-        reject(this.errorValue);
-        this.isRunning = false;
-        this.pullQueue.length = 0;
-        this.pushQueue.length = 0;
+      if (this.errorValue !== NO_ERROR_SYMBOL) {
+        this._handleError(reject);
         return;
       }
       resolve(
@@ -63,13 +108,5 @@ export class PushPullAsyncIterableIterator<T>
     } else {
       this.pushQueue.push(value);
     }
-  }
-
-  public throw(value: unknown): Promise<IteratorResult<T>> {
-    this.errorValue = value;
-    return Promise.resolve({
-      done: true,
-      value: undefined
-    });
   }
 }
